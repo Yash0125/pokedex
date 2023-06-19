586bef7771c48a1a1456dc0973fae2dcc85753c0
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-06-19 21:23:26 +0530

    refactor : PokemonProvider

--- src/context/PokemonProvider.js
+++ src/context/PokemonProvider.js
@@ -1,22 +1,149 @@
+import { useEffect, useState } from "react";
 import { useForm } from "../hook/useForm";
 import { PokemonContext } from "./PokemonContext";
 
 export const PokemonProvider = ({ children }) => {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [globalPokemons, setGlobalPokemons] = useState([]);
+  const [offset, setOffset] = useState(0);
+
   // Use CustomHook - useForm
   const { valueSearch, onInputChange, onResetForm } = useForm({
-    valueSearch: "",
+    valueSearch: ""
   });
+
+  // Simple application states
+  const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState(false);
+
+  // call 10 pokemon to api
+  const getAllPokemons = async (limit = 10) => {
+    const baseURL = "https://pokeapi.co/api/v2/";
+
+    const res = await fetch(
+      `${baseURL}pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await res.json();
+
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+    const results = await Promise.all(promises);
+
+    setAllPokemons([...allPokemons, ...results]);
+    setLoading(false);
+  };
+
+  // call all pokemon
+  const getGlobalPokemons = async () => {
+    const baseURL = "https://pokeapi.co/api/v2/";
+
+    const res = await fetch(`${baseURL}pokemon/`);
+    const data = await res.json();
+
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+    const results = await Promise.all(promises);
+
+    setGlobalPokemons(results);
+    setLoading(false);
+  };
+
+  // call a pokemon by ID
+  const getPokemonByID = async (id) => {
+    const baseURL = "https://pokeapi.co/api/v2/";
+
+    const res = await fetch(`${baseURL}pokemon/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, [offset]);
+
+  useEffect(() => {
+    getGlobalPokemons();
+  }, []);
+
+  // BTN LOAD MORE
+  const onClickLoadMore = () => {
+    setOffset(offset + 10);
+  };
+
+  // Filter Function + State
+  const [typeSelected, setTypeSelected] = useState({
+    grass: false,
+    normal: false,
+    fighting: false,
+    flying: false,
+    poison: false,
+    ground: false,
+    rock: false,
+    bug: false,
+    ghost: false,
+    steel: false,
+    fire: false,
+    water: false,
+    electric: false,
+    psychic: false,
+    ice: false,
+    dragon: false,
+    dark: false,
+    fairy: false,
+    unknow: false,
+    shadow: false
+  });
+
+  const [filteredPokemons, setfilteredPokemons] = useState([]);
+
+  const handleCheckbox = (e) => {
+    setTypeSelected({
+      ...typeSelected,
+      [e.target.name]: e.target.checked
+    });
+
+    if (e.target.checked) {
+      const filteredResults = globalPokemons.filter((pokemon) =>
+        pokemon.types.map((type) => type.type.name).includes(e.target.name)
+      );
+      setfilteredPokemons([...filteredPokemons, ...filteredResults]);
+    } else {
+      const filteredResults = filteredPokemons.filter(
+        (pokemon) =>
+          !pokemon.types.map((type) => type.type.name).includes(e.target.name)
+      );
+      setfilteredPokemons([...filteredResults]);
+    }
+  };
 
   return (
     <PokemonContext.Provider
       value={{
         valueSearch,
         onInputChange,
-        onResetForm
+        onResetForm,
+        allPokemons,
+        globalPokemons,
+        getPokemonByID,
+        onClickLoadMore,
+        // Loader
+        loading,
+        setLoading,
+        // Btn Filter
+        active,
+        setActive,
+        // Filter Container Checkbox
+        handleCheckbox,
+        filteredPokemons
       }}
     >
       {children}
     </PokemonContext.Provider>
   );
 };
-// onInputChange, valueSearch, onResetForm
