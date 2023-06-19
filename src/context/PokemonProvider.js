e2c99919427085e2f6ce39efc6a479c9ad768c11
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-06-19 21:12:08 +0530

    feat : Navigation

--- src/context/PokemonProvider.js
+++ src/context/PokemonProvider.js
@@ -1,6 +1,22 @@
+import { useForm } from "../hook/useForm";
 import { PokemonContext } from "./PokemonContext";
 
 export const PokemonProvider = ({ children }) => {
-  return (<PokemonContext.Provider value={{}}></PokemonContext.Provider>
+  // Use CustomHook - useForm
+  const { valueSearch, onInputChange, onResetForm } = useForm({
+    valueSearch: "",
+  });
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        valueSearch,
+        onInputChange,
+        onResetForm
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
   );
 };
+// onInputChange, valueSearch, onResetForm
