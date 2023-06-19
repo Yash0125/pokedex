179a623787201e1d3932489045634cb6778913a4
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-06-19 20:55:45 +0530

    refactor : Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import HomePage from "./pages/HomePage";
+import PokemonPage from "./pages/PokemonPage";
+import SearchPage from "./pages/SearchPage";
+import BookmarkPage from "./pages/BookmarkPage";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<HomePage />} />
+            <Route path="pokemon/:id" element={<PokemonPage />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route path="bookmark" element={<BookmarkPage />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
